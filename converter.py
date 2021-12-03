180be8309475eb50f4758359d3f76a8f84c44a5b
dimkas71g@gmail.com <dimkas71g@gmail.com>
2021-12-03 17:47:11 +0200

    Added save xml information to the docx file

--- converter.py
+++ converter.py
@@ -1,8 +1,12 @@
 from datetime import datetime 
 from datetime import date
+from posixpath import pardir
 
 from xml.etree import ElementTree as ET
 from dataclasses import dataclass
+
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH 
 
 @dataclass
 class HealthInfo:
@@ -44,5 +48,40 @@ def load_health_info(path: str)->list[HealthInfo]:
         health_info.append(HealthInfo(health_number,ident_number,first_name,second_name,surname,begin_date, end_date))
     return health_info           
 
+def save_health_info(source:str, target:str) -> None:
+   document = Document()
+   p = document.add_paragraph('РЕЄСТР')
+   p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+   p = document.add_paragraph('листків непрацездатності, які передані зі служби управління персоналом')
+   p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+   p = document.add_paragraph('до комісії зі соціального страхування КП МТК <<Калинівський ринок>>')
+   p.alignment = WD_ALIGN_PARAGRAPH.CENTER
+
+   health_info = load_health_info(source)
+
+   table = document.add_table(rows=1, cols=4)
+   table_header = table.rows[0].cells
+   table_header[0].text = '№ з/п'
+   table_header[1].text = 'Прізвище,ім\'я, по батькові'
+   table_header[2].text = '№ листка непрацездатності'
+   table_header[3].text = 'Номер страхового свідоцтва'
+
+   for index, r in enumerate(health_info):
+       row = table.add_row().cells
+       row[0].text = f"{index+1}"
+       row[1].text = f"{r.first_name} {r.second_name} {r.surname}"
+       row[2].text = f"{r.health_number}"
+       row[3].text = f"{r.ident_number}"
+    
+   p = document.add_paragraph(f"Листки непрацездатності в кількості {len(health_info)} штуки")
+   p = document.add_paragraph(f"Передала:              Прийняв:") 
+   p = document.add_paragraph(f"Начальник служби       Голова комісії із соціального") 
+   p = document.add_paragraph(f"управління персоналом  страхування") 
+   
+   document.save(target)
+
+
 if (__name__ == '__main__'):
-    print(load_health_info('export.xml'))
+   save_health_info('export.xml', 'reestr.docx') 
