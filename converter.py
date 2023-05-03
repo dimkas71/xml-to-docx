e9c52ceed681ca330651f09256491b9ff6977647
dimkas71g@gmail.com <dimkas71g@gmail.com>
2023-05-03 14:31:08 +0300

    Minor fixes

--- converter.py
+++ converter.py
@@ -31,19 +31,19 @@ def load_health_info(path: str) -> list[HealthInfo]:
         begin_date: date = date(1, 1, 1)
         end_date: date = date(1, 1, 1)
         for child in e:
-            if (child.tag == 'WIC_NUM'):
+            if child.tag == 'WIC_NUM':
                 health_number = child.text
-            if (child.tag == 'NP_NUMIDENT'):
+            if child.tag == 'NP_NUMIDENT':
                 ident_number = child.text
-            if (child.tag == 'NP_SURNAME'):
+            if child.tag == 'NP_SURNAME':
                 first_name = child.text
-            if (child.tag == 'NP_NAME'):
+            if child.tag == 'NP_NAME':
                 second_name = child.text
-            if (child.tag == 'NP_PATRONYMIC'):
+            if child.tag == 'NP_PATRONYMIC':
                 surname = child.text
-            if (child.tag == 'WIC_DT_BEGIN'):
+            if child.tag == 'WIC_DT_BEGIN':
                 begin_date = datetime.strptime(child.text, "%Y-%m-%dT%H:%M:%S")
-            if (child.tag == 'WIC_DT_END'):
+            if child.tag == 'WIC_DT_END':
                 end_date = datetime.strptime(child.text, "%Y-%m-%dT%H:%M:%S")
         health_info.append(
             HealthInfo(health_number, ident_number, first_name, second_name, surname, begin_date, end_date))
